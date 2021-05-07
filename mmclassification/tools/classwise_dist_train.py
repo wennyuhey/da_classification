dc80a1207a40b1c68f88615c42fad0584ebaed0b
yu <wennyuhey@gmail.com>
2021-05-07 17:56:59 +0800

    .

--- mmclassification/tools/classwise_dist_train.py
+++ mmclassification/tools/classwise_dist_train.py
@@ -6,5 +6,6 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 variable=${@:3}
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#CUDA_VISIBLE_DEVICES=0,1,2,3 
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/classwise_train.py $CONFIG --launcher pytorch ${@:3}
